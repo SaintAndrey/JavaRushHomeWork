a26f8c158e9f719ce578a7690a09b980f908da64
Andrey <andrersv96@gmail.com>
2016-11-02 18:46:56 +0300

    Добавил задачу

---
+++ src/com/javarush/test/level14/lesson08/home01/Bridge.java
@@ -0,0 +1,8 @@
+package com.javarush.test.level14.lesson08.home01;
+
+/**
+ * Created by Andrey on 02/11/2016.
+ */
+public interface Bridge {
+    int getCarsCount();
+}
---
+++ src/com/javarush/test/level14/lesson08/home01/SuspensionBridge.java
@@ -0,0 +1,11 @@
+package com.javarush.test.level14.lesson08.home01;
+
+/**
+ * Created by Andrey on 02/11/2016.
+ */
+public class SuspensionBridge implements Bridge{
+    @Override
+    public int getCarsCount() {
+        return 12;
+    }
+}
